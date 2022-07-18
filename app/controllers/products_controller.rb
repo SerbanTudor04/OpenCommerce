9aee9b751375b130bde0b5318705fa60e5a4db7c
SerbanTudor04 <tudor.sgabriel.serban@gmail.com>
2022-07-18 18:01:51 +0300

    Fix products API by putting the corect model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,10 @@
 class ProductsController < ApplicationController
 
     def getProductsByCategory
-        @products = Product.where(:category_id=>params[:category_id])
+        @products = OcomProduct.where(:category_id=>params[:category_id])
         render json: @products
     end
 
-
-    private
-
-    def product_params
-        params.require(:product).permit(:name, :description, :price, :category_id, :image)
-    end
-
-    def set_product
-        @product = Product.find(params[:id])
-    end
 
 
 end
